e8947c2a9b1a2755f5a8267ce34de8da32c7cc91
NeKosmic-OFC <101365916+NeKosmic-OFC@users.noreply.github.com>
2022-03-20 22:25:51 +0000

    Update panties.js

--- plugins/panties.js
+++ plugins/panties.js
@@ -5,6 +5,6 @@ let json = res.data
 let url = json.url
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let mentionedJid = [who]
-conn.send2ButtonImg(m.chat, url, "*Panties*", '©The Shadow Borkers - Bot', '🥵 SIGUIENTE 🥵', `${usedPrefix + command}`, '🔥 LABIBLIA 🔥', `${usedPrefix}labiblia`, m, false, { contextInfo: { mentionedJid }}) }
+conn.send2ButtonImg(m.chat, url, "*Panties*", '™𝓝𝓮𝓚𝓸𝓼𝓶𝓲𝓬 - 𝓑𝓞𝓣', '🥵 SIGUIENTE 🥵', `${usedPrefix + command}`, '🔥 LABIBLIA 🔥', `${usedPrefix}labiblia`, m, false, { contextInfo: { mentionedJid }}) }
 handler.command = /^(panties)$/i
 module.exports = handler
