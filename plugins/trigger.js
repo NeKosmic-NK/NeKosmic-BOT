b08fdd5e3bbfa40e5a6ea736e6dbb4819a97674c
NeKosmic-OFC <101365916+NeKosmic-OFC@users.noreply.github.com>
2022-03-20 23:16:31 +0000

    Update trigger.js

--- plugins/trigger.js
+++ plugins/trigger.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn }) => {
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let wanted = global.API('https://some-random-api.ml', '/canvas/triggered', {
 avatar: await conn.getProfilePicture(who).catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),})
-let stiker = await sticker(null, wanted, 'Trigger', '©The Shadow Brokers - Bot')
+let stiker = await sticker(null, wanted, 'Trigger', '™𝓝𝓮𝓚𝓸𝓼𝓶𝓲𝓬 - 𝓑𝓞𝓣')
 conn.sendMessage(m.chat, stiker, MessageType.sticker, {
 quoted: m})}
 handler.command = /^(trigger)$/i
