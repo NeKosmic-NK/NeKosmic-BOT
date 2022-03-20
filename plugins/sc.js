5aa3b07b42f98bc080a79007b43a71d83b76d80d
NeKosmic-OFC <101365916+NeKosmic-OFC@users.noreply.github.com>
2022-03-20 22:53:37 +0000

    Update sc.js

--- plugins/sc.js
+++ plugins/sc.js
@@ -9,7 +9,7 @@ conn.reply(m.chat, text, m, { thumbnail: kai, contextInfo: {
 mentionedJid: [who],
 externalAdReply: {
 title: '𝚁𝙴𝙿𝙾𝚂𝙸𝚃𝙾𝚁𝙸𝙾 𝙳𝙴𝙻 𝙱𝙾𝚃',
-body: '🤖 Ｓｈａｄｏｗ Ｂｏｔ 🤖',
+body: '🤖 N̶̛͋̄̊̓̇͐̂͠e̷̖̩̾̆̾̓̾̓̂K̴̙̭̪̅͑̿̚ó̷̙͔͒̉̆̈́͝͝s̴̼͎̝̺͔͈̈̄̾m̸̢͕̒̒̃́͘͝͠į̷͚̮̩̂̍͆͜c̵͙̼̈̾̍͛̉̈́̚ - B̵̠̤̘̜̈́͝O̷̍͂͒̏̒͋̈́̐̅Ṫ̵̹̓̌̀̕͝͝͝ 🤖',
 thumbnail: pp
 }}})}
 handler.command = /^sc|script|repositorio|repobot|scbot$/i
