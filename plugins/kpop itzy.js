88e885b95307d47ce70e2e47d336e594f2e38483
NeKosmic-OFC <101365916+NeKosmic-OFC@users.noreply.github.com>
2022-03-20 21:31:19 +0000

    Update kpop itzy.js

--- plugins/kpop itzy.js
+++ plugins/kpop itzy.js
@@ -2,9 +2,9 @@ const axios = require('axios')
 let handler = async(m, { conn, usedPrefix, command }) => {
 let res = await axios("https://meme-api.herokuapp.com/gimme/Itzy")
 let json = res.data
-let ShadowBot = json.url
+let NeKosmicBot = json.url
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let mentionedJid = [who]
-conn.sendButtonImg(m.chat, ShadowBot, "*✨Itzy✨*", '©The Shadow Borkers - Bot', 'SIGUIENTE', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
+conn.sendButtonImg(m.chat, NeKosmicBot, "*✨Itzy✨*", '™𝓝𝓮𝓚𝓸𝓼𝓶𝓲𝓬 - 𝓑𝓞𝓣', '🎼SIGUIENTE🎼', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
 handler.command = /^(kpopitzy|itzy)$/i
 module.exports = handler
