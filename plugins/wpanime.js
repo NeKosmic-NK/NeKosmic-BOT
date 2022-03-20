fd16c8c97b11063c8f6e3caf4c776f6cc84c49fd
NeKosmic-OFC <101365916+NeKosmic-OFC@users.noreply.github.com>
2022-03-20 23:22:05 +0000

    Update wpanime.js

--- plugins/wpanime.js
+++ plugins/wpanime.js
@@ -6,7 +6,7 @@ let handler = async (m, { conn }) => {
         let res = await fetch(global.API('xteam', '/randomimage/wpmobile', {}, 'APIKEY'))
         let img = await res.buffer()
         conn.sendMessage(m.chat, img, MessageType.image, {
-            quoted: m, caption: '*✨The Shadow Brokers - Bot✨*'
+            quoted: m, caption: '*✨𝓝𝓮𝓚𝓸𝓼𝓶𝓲𝓬 - 𝓑𝓞𝓣✨*'
         })
     } catch (e) {
         console.log(e)
