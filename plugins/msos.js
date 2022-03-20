88dbc0fcca21ac136c181c40d6bd4ea12ee3b70d
NeKosmic-OFC <101365916+NeKosmic-OFC@users.noreply.github.com>
2022-03-20 22:33:46 +0000

    Update msos.js

--- plugins/msos.js
+++ plugins/msos.js
@@ -5,5 +5,5 @@ let fetch = require('node-fetch')
    conn.sendButtonImg(m.chat, json, '*By NeKosmicBot*\n*https://api-alc.herokuapp.com/api/chicas/muslos?&apikey=ConfuMods*', '™𝓝𝓮𝓚𝓸𝓼𝓶𝓲𝓬 - 𝓑𝓞𝓣', 'SIGUIENTE', `${usedPrefix + command}`, m, false)
 }
 handler.command = /^(muslitos)$/i
-
+handler.admin = true
 module.exports = handler
