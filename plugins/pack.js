2b193deb2e37bf7e358b4a641de9ff495253e046
NeKosmic-OFC <101365916+NeKosmic-OFC@users.noreply.github.com>
2022-03-20 22:38:56 +0000

    Update pack.js

--- plugins/pack.js
+++ plugins/pack.js
@@ -15,6 +15,7 @@ let handler = async (m, { conn }) => {
 handler.command = /^(pack)$/i
 handler.tags = ['internet']
 handler.help = ['pack']
+handler.admin = true
 module.exports = handler
 
 global.pack = [
