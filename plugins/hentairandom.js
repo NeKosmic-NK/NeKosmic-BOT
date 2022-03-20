81d04ba2a600113312ba306069d3960704a5c071
NeKosmic-OFC <101365916+NeKosmic-OFC@users.noreply.github.com>
2022-03-20 21:15:02 +0000

    Update hentairandom.js

--- plugins/hentairandom.js
+++ plugins/hentairandom.js
@@ -2,7 +2,7 @@ let fetch = require('node-fetch')
      let handler  = async (m, { conn, usedPrefix, command }) => {
     heum = await fetch(`https://server-api-rey.herokuapp.com/api/nsfw/hentai?apikey=apirey`)
     json = await heum.buffer()
-   conn.sendButtonImg(m.chat, json, '*Hentai By ShadowBot*', '©The Shadow Brokers - Bot', 'SIGUIENTE', `${usedPrefix + command}`, m, false)
+   conn.sendButtonImg(m.chat, json, '*Hentai By NeKosmicBot*', '™𝓝𝓮𝓚𝓸𝓼𝓶𝓲𝓬 - 𝓑𝓞𝓣', '🔥SIGUIENTE🔥', `${usedPrefix + command}`, m, false)
 }
 handler.command = /^(hentai|hentay)$/i
 
