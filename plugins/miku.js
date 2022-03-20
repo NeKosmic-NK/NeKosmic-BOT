165a134c1945fc3f3aa4f5a0868be8fd3f7268b7
NeKosmic-OFC <101365916+NeKosmic-OFC@users.noreply.github.com>
2022-03-20 22:09:05 +0000

    Update miku.js

--- plugins/miku.js
+++ plugins/miku.js
@@ -2,7 +2,7 @@ let fetch = require('node-fetch')
      let handler  = async (m, { conn, usedPrefix, command }) => {
     heum = await fetch(`https://api-reysekha.herokuapp.com/api/wallpaper/miku?apikey=APIKEY`)
     json = await heum.buffer()
-   conn.sendButtonImg(m.chat, json, '*Hatsune Miku*', '©The Shadow Brokers - Bot', 'SIGUIENTE', `${usedPrefix + command}`, m, false)
+   conn.sendButtonImg(m.chat, json, '*Hatsune Miku*', '™𝓝𝓮𝓚𝓸𝓼𝓶𝓲𝓬 - 𝓑𝓞𝓣', '👑SIGUIENTE👑', `${usedPrefix + command}`, m, false)
 }
 handler.command = /^(micu|miku)$/i
 
