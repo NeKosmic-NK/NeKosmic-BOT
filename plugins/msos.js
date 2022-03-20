bfc5e2ee93d64375ed2bf93551a89f8107e8a279
NeKosmic-OFC <101365916+NeKosmic-OFC@users.noreply.github.com>
2022-03-20 22:11:21 +0000

    Update msos.js

--- plugins/msos.js
+++ plugins/msos.js
@@ -2,7 +2,7 @@ let fetch = require('node-fetch')
      let handler  = async (m, { conn, usedPrefix, command }) => {
     heum = await fetch(`https://api-alc.herokuapp.com/api/chicas/muslos?&apikey=ConfuMods`)
     json = await heum.buffer()
-   conn.sendButtonImg(m.chat, json, '*By ShadowBot and ConfuMods*\n*https://api-alc.herokuapp.com/api/chicas/muslos?&apikey=ConfuMods*', '©The Shadow Brokers - Bot', 'SIGUIENTE', `${usedPrefix + command}`, m, false)
+   conn.sendButtonImg(m.chat, json, '*By NeKosmicBot*\n*https://api-alc.herokuapp.com/api/chicas/muslos?&apikey=ConfuMods*', '™𝓝𝓮𝓚𝓸𝓼𝓶𝓲𝓬 - 𝓑𝓞𝓣', 'SIGUIENTE', `${usedPrefix + command}`, m, false)
 }
 handler.command = /^(muslitos)$/i
 
