42a1a2e16a57e675b3b0dfd86d68a7f15dd12505
NeKosmic-OFC <101365916+NeKosmic-OFC@users.noreply.github.com>
2022-03-20 23:10:46 +0000

    Update terminos y condiciones.js

--- plugins/terminos y condiciones.js
+++ plugins/terminos y condiciones.js
@@ -21,37 +21,22 @@ _2.- No nos hacemos responsables de la ignorancia y/o desconocimiento del tema_
 _3.- El Bot no esta activo las 24 horas al menos de que el propietario decida lo contrario_
 _4.- La "empresa" no se hace responsable del uso de un Sub Bot o Bot no oficial, ya que son personas (terceros) y no tenemos control sobre ell@s_
 _5.- No nos hacemos responsables de los números que se puedan ir a soporte por el uso del Bot, es aconsejable siempre usar números virtuales para usos de Bots_
-_6.- Los audios/notas de voz/imágenes/videos o cualquier otro archivo multimedia que sean de propiedad de The Shadow Brokers - Bot son únicamente y exclusivamente de este Bot, si se detecta a otro Bot con alguno o algunos de los archivos multimedia antes mencionado se tomarán cartas en el asunto._
+_6.- Los audios/notas de voz/imágenes/videos o cualquier otro archivo multimedia que sean de propiedad de NeKosmic - Bot son únicamente y exclusivamente de este Bot, si se detecta a otro Bot con alguno o algunos de los archivos multimedia antes mencionado se tomarán cartas en el asunto._
 
 *➤ Mensaje del numero del Bot?*
 
 _- Si en algún momento recibe un mensaje del numero del Bot y no es un comando posiblemente sea de el propietario del Bot o el propietario del numero, este es un persona real por lo que se le pide que no le falte el respeto_
 
-*➤ Base usada para este Bot:*
-*- https://github.com/BochilGaming/games-wabot*
-
-*Agradecimientos a:*
-*- Unx21*
-*- Narutomo*
-*- BochilGaming*
-*- Unxteam*
-*- Bochilteam*
-*- ConfuMods*
-*- Hiro*
-*- The.sad.boy01*
-*- botstylee*
-*- Amel*
-
 *⁉️ Te quedaron dudas?*
 
 _- Si todavía tienes alguna duda o reclamo de lo mencionado aqui o referente al Bot, contáctame a mi numero personal al cual estaré respondiendo lo antes posible para brindarte una solución y/o hacerme responsable de alguna reclamación_ 
-> wa.me/5219996125657
-> Bruno Sobrino 
-> Correo electronico: theshadowbrokers133@gmailcom
+> wa.me/+34623442554
+> ¥Alpha-ØFC¥
+> Correo electronico: nekosmic.bot@gmail.com 
 
 → *Solo contactar por temas serios, para preguntas del Bot mas generales contáctame al privado con el comando del comando #owner (al numero wa.me/17722386341)*
 
-*∆ THE SHADOW BROKERS - BOT ∆*
+*∆ NEKOSMIC - BOT ∆*
 `.trim(), m)
 let mentionedJid = [m.sender]} 
 handler.customPrefix = /términos y condiciones y privacidad|terminosycondicionesyprivacidad|terminosycondiciones|terminos y condiciones y privacidad|terminos y condiciones|terminos y condiciones|terminos de uso|Terminos de uso|Terminó se uso|términos de uso|Términos de uso|Términos y condiciones/i
