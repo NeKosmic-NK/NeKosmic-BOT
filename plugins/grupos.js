1c5e3aff0c09c399ea34476b2fa0382dbddd1e06
NeKosmic-OFC <101365916+NeKosmic-OFC@users.noreply.github.com>
2022-03-20 21:13:07 +0000

    Update grupos.js

--- plugins/grupos.js
+++ plugins/grupos.js
@@ -6,25 +6,15 @@ let info = `
 *Hola 👋🏻, unete a los grupos oficiales para pasar un rato agradable usando el Bot o platicando con la familia de The Shadow Borkers - Bot*
 
 *➤ Grupos oficiales del Bot:*
-*1.-* https://chat.whatsapp.com/JlomZPEgo3bLmzjGUYPfyJ
+*1.-* *Soporte Técnico:* https://chat.whatsapp.com/FJAu5KYaawU5ty9apbdFmy
 
-*2.-* https://chat.whatsapp.com/DbXBmsydWBE1ZN3EoY0hRs
+*2.-* *Familia Otaku:* https://chat.whatsapp.com/IVE30IhKwxsGbOunDFpOS2
 
-*3.-* https://chat.whatsapp.com/BW0P22xx7EGBTdH5IM851F
+*3.-* https://chat.whatsapp.com/Grupo-No-Creado
 
-*4.-* https://chat.whatsapp.com/CjexkGVr37J6GuSdDVAHzC
+*4.-* https://chat.whatsapp.com/Grupo-No-Creado
 
-*5.-* https://chat.whatsapp.com/H7NUXdrGlFg20ae3bqgwlb
-
-*6.-* https://chat.whatsapp.com/GvrLSUSzVuT9P17CKfdxDa
-
-*7.-* https://chat.whatsapp.com/HylyPaLc49b9DWF67K8pdc
-
-*8.-* https://chat.whatsapp.com/IW12dLVoyWGHreGpX7rQIw
-
-*9.-* https://chat.whatsapp.com/Ef89aIuOLeD3Fa2sYmKwp7
-
-*10.-* https://chat.whatsapp.com/C2WYb1hiiijCI13QSUZLM9
+*5.-* https://chat.whatsapp.com/Grupo-No-Creado
 `.trim() 
 
 conn.sendMessage(m.chat, info, MessageType.text, { quoted: { key: { remoteJid: 'status@broadcast', participant: '0@s.whatsapp.net', fromMe: false }, message: { "imageMessage": { "mimetype": "image/jpeg", "caption": '🔥 THE SHADOW BROKERS - BOT 🔥', "jpegThumbnail": fs.readFileSync(`./Menu2.jpg`)}}}})}
