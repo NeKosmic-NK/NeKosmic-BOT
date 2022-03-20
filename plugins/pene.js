ea0376bf9f3bde572f65a6287b41d8c84edc9875
NeKosmic-OFC <101365916+NeKosmic-OFC@users.noreply.github.com>
2022-03-20 22:40:53 +0000

    Update pene.js

--- plugins/pene.js
+++ plugins/pene.js
@@ -7,4 +7,5 @@ let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? c
 let mentionedJid = [who]
 conn.sendButtonImg(m.chat, NeKosmicBot, "*Pene 🍆*", '™𝓝𝓮𝓚𝓸𝓼𝓶𝓲𝓬 - 𝓑𝓞𝓣', '🥵 SIGUIENTE 🥵', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
 handler.command = /^(pene)$/i
+handler.admin = true
 module.exports = handler
