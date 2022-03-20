d95213b2e7312c1494c755c7d7c3c1df6285c105
NeKosmic-OFC <101365916+NeKosmic-OFC@users.noreply.github.com>
2022-03-20 22:30:41 +0000

    Update hentairandom.js

--- plugins/hentairandom.js
+++ plugins/hentairandom.js
@@ -5,5 +5,5 @@ let fetch = require('node-fetch')
    conn.sendButtonImg(m.chat, json, '*Hentai By NeKosmicBot*', '™𝓝𝓮𝓚𝓸𝓼𝓶𝓲𝓬 - 𝓑𝓞𝓣', '🔥SIGUIENTE🔥', `${usedPrefix + command}`, m, false)
 }
 handler.command = /^(hentai|hentay)$/i
-
+handler.admin = true
 module.exports = handler
