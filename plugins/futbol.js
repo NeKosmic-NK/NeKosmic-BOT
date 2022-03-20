c02f729e8ad330ad06e7700ce4b722c8c62979f8
NeKosmic-OFC <101365916+NeKosmic-OFC@users.noreply.github.com>
2022-03-20 21:07:36 +0000

    Update futbol.js

--- plugins/futbol.js
+++ plugins/futbol.js
@@ -2,9 +2,9 @@ const axios = require('axios')
 let handler = async(m, { conn, usedPrefix, command }) => {
 let res = await axios("https://meme-api.herokuapp.com/gimme/futbol")
 let json = res.data
-let ShadowBot = json.url
+let NeKosmicBot = json.url
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let mentionedJid = [who]
-conn.sendButtonImg(m.chat, ShadowBot, "*Fútbol 🥅*", '©The Shadow Borkers - Bot', '⚽ SIGUIENTE ⚽', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
+conn.sendButtonImg(m.chat, NeKosmicBot, "*Fútbol 🥅*", '™𝓝𝓮𝓚𝓸𝓼𝓶𝓲𝓬 - 𝓑𝓞𝓣', '⚽ SIGUIENTE ⚽', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
 handler.command = /^(futbol|fútbol)$/i
 module.exports = handler
