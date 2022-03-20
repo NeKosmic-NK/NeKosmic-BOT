0b2cd38708d765b2c19f26255fd77d6eefd00873
NeKosmic-OFC <101365916+NeKosmic-OFC@users.noreply.github.com>
2022-03-20 22:22:04 +0000

    Update nyan.js

--- plugins/nyan.js
+++ plugins/nyan.js
@@ -9,7 +9,7 @@ let handler = async (m, { conn}) => {
   let { 
 url
 } = json
-let stiker = await sticker(null, url, 'Nyan', '©The Shadow Brokers - Bot')
+let stiker = await sticker(null, url, 'Nyan', '™𝓝𝓮𝓚𝓸𝓼𝓶𝓲𝓬 - 𝓑𝓞𝓣')
   conn.sendMessage(m.chat, stiker, MessageType.sticker, {
     quoted: m
   })
