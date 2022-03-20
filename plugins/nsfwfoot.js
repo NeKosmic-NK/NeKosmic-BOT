ae44f2e82ac5a92f2bb85cf801cad96a1edf1998
NeKosmic-OFC <101365916+NeKosmic-OFC@users.noreply.github.com>
2022-03-20 22:36:40 +0000

    Update nsfwfoot.js

--- plugins/nsfwfoot.js
+++ plugins/nsfwfoot.js
@@ -5,5 +5,5 @@ let fetch = require('node-fetch')
    conn.sendButtonImg(m.chat, json, '*Disfrutalo!!*', '™𝓝𝓮𝓚𝓸𝓼𝓶𝓲𝓬 - 𝓑𝓞𝓣', '👑SIGUIENTE👑', `${usedPrefix + command}`, m, false)
 }
 handler.command = /^(foot|nsfwfoot)$/i
-
+handler.admin = true
 module.exports = handler
