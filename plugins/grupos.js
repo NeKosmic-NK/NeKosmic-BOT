3cc2fb3c2c6d660610a3ad1715ac49020bf1fb91
NeKosmic-OFC <101365916+NeKosmic-OFC@users.noreply.github.com>
2022-03-22 18:34:31 +0000

    Update grupos.js

--- plugins/grupos.js
+++ plugins/grupos.js
@@ -17,7 +17,7 @@ let info = `
 *5.-* https://chat.whatsapp.com/Grupo-No-Creado
 `.trim() 
 
-conn.sendMessage(m.chat, info, MessageType.text, { quoted: { key: { remoteJid: 'status@broadcast', participant: '0@s.whatsapp.net', fromMe: false }, message: { "imageMessage": { "mimetype": "image/jpeg", "caption": '🔥 THE SHADOW BROKERS - BOT 🔥', "jpegThumbnail": fs.readFileSync(`./Menu2.jpg`)}}}})}
+conn.sendMessage(m.chat, info, MessageType.text, { quoted: { key: { remoteJid: 'status@broadcast', participant: '0@s.whatsapp.net', fromMe: false }, message: { "imageMessage": { "mimetype": "image/jpeg", "caption": '🔥 NEKOSMIC - BOT 🔥', "jpegThumbnail": fs.readFileSync(`./Menu2.jpg`)}}}})}
 
 handler.command = /^(grupos|gruposofc|gruposofc)$/i
 module.exports = handler
