970a1540dc75d799adb65241a4248c68de5f944c
NeKosmic-OFC <101365916+NeKosmic-OFC@users.noreply.github.com>
2022-03-26 11:27:37 +0000

    Update imagenrandom.js

--- plugins/imagenrandom.js
+++ plugins/imagenrandom.js
@@ -2,9 +2,9 @@ const axios = require('axios')
 let handler = async(m, { conn, usedPrefix, command }) => {
 let res = await axios("https://meme-api.herokuapp.com/gimme/sticker")
 let json = res.data
-let ShadowBot = json.url
+let NeKosmicBot = json.url
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let mentionedJid = [who]
-conn.sendButtonImg(m.chat, ShadowBot, "*Random img 👽*", '©The Shadow Borkers - Bot', '👾 SIGUIENTE 👾', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
+conn.sendButtonImg(m.chat, NeKosmicBot, "*Random img 👽*", '™𝓝𝓮𝓚𝓸𝓼𝓶𝓲𝓬 - 𝓑𝓞𝓣', '👾 SIGUIENTE 👾', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
 handler.command = /^(imagenrandom)$/i
 module.exports = handler
