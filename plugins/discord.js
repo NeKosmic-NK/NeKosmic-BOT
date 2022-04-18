735464a2ca912ecc1274813162304694f9c1382f
NeKosmic-OFC <101365916+NeKosmic-OFC@users.noreply.github.com>
2022-04-18 22:55:33 +0100

    Update discord.js

--- plugins/discord.js
+++ plugins/discord.js
@@ -1,20 +1,8 @@
-let PhoneNumber = require('awesome-phonenumber')
-let levelling = require('../lib/levelling')
-
-let handler = async (m, { conn, usedPrefix }) => {
-
-  let pp = './menu.jpg'
-  let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-  try {
-//    pp = await conn.getProfilePicture(who)
-  } catch (e) {
-
-  } finally {
-    let about = (await conn.getStatus(who).catch(console.error) || {}).status || ''
-    let { name, limit, exp, banned, lastclaim, registered, regTime, age, level } = global.DATABASE.data.users[m.sender]
-    let { min, xp, max } = levelling.xpRange(level, global.multiplier)
-    let username = conn.getName(who)
-    let str = `
+let { MessageType } = require('@adiwajshing/baileys')
+let fs = require('fs')
+let handler  = async (m, { conn, usedPrefix: _p }) => {
+  
+let info = `
 ─━─━─━∞◆∞━─━─━─┓
 ༆⋆⃟⋆⃟:🍥:LATĀM:🍥:eᬽ⃤
 ┗─━─━─━∞◆∞━─━─━─┛
