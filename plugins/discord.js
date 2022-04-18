98c611c8a3bcaef19649e6fa5c2235d802623902
NeKosmic-OFC <101365916+NeKosmic-OFC@users.noreply.github.com>
2022-04-18 22:50:17 +0100

    Update discord.js

--- plugins/discord.js
+++ plugins/discord.js
@@ -34,5 +34,5 @@ let info = `
 
 conn.sendMessage(m.chat, info, MessageType.text, { quoted: { key: { remoteJid: 'status@broadcast', participant: '0@s.whatsapp.net', fromMe: false }, message: { "imageMessage": { "mimetype": "image/jpeg", "caption": '🔥 NEKOSMIC - BOT 🔥', "jpegThumbnail": fs.readFileSync(`./menu.jpg`)}}}})}
 
-handler.command = /^(grupos|gruposofc|gruposofc)$/i
+handler.command = /^(discord|Discord|discord-server)$/i
 module.exports = handler
