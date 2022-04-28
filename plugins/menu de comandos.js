50ab59560ce495a0643d2b15252ae6c0b7fef3d0
NeKosmic-NK <104524366+NeKosmic-NK@users.noreply.github.com>
2022-04-28 20:44:32 +0100

    Update menu de comandos.js

--- plugins/menu de comandos.js
+++ plugins/menu de comandos.js
@@ -9,7 +9,7 @@ let moment = require('moment-timezone')
 let path = require('path')
 let util = require('util')
 let handler = async (m, { conn, usedPrefix }) => {
-let pp = './NeKo.jpg'
+let pp = './img1.jpeg'
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let username = conn.getName(who)
 //let vn = './media/mariana.mp3'
