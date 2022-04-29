ae1c519ff07ddf8764dc1e4749376071e1c23a49
NeKosmic-NK <104524366+NeKosmic-NK@users.noreply.github.com>
2022-04-29 23:52:10 +0100

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -2,6 +2,6 @@ function handler(m) {
 this.sendContact(m.chat, global.owner[0], this.getName(global.owner[0] + '@s.whatsapp.net'), m)
 this.sendContact(m.chat, '34623442554', 'NeKosmic - Owner', m)
 this.sendContact(m.chat, '97255-723-4456', 'NeKosmic - Creador - Oficial 2', m)
-sendContact(m.chat, '97255-704-9012', 'NeKosmic - BOT', m)}
+sendContact(m.chat, '97255-704-9012', 'NeKosmic - BOT', m)
 handler.command = /^(contacto|owner|creator|creador|propietario|dueño)$/i
 module.exports = handler
