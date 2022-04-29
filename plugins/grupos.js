dc61c0221855bce3b1e074a98927a2acd9162a93
NeKosmic-NK <104524366+NeKosmic-NK@users.noreply.github.com>
2022-04-29 09:27:12 +0100

    Update grupos.js

--- plugins/grupos.js
+++ plugins/grupos.js
@@ -21,7 +21,7 @@ https://chat.whatsapp.com/LINRVAQIhZdEAlShmLmhK5
 *5.-* https://chat.whatsapp.com/Grupo-No-Creado
 `.trim() 
 
-conn.sendMessage(m.chat, info, MessageType.text, { quoted: { key: { remoteJid: 'status@broadcast', participant: '0@s.whatsapp.net', fromMe: false }, message: { "imageMessage": { "mimetype": "image/jpeg", "caption": '🔥 NEKOSMIC - BOT 🔥', "jpegThumbnail": fs.readFileSync(`./menu.jpg`)}}}})}
+conn.sendMessage(m.chat, info, MessageType.text, { quoted: { key: { remoteJid: 'status@broadcast', participant: '0@s.whatsapp.net', fromMe: false }, message: { "imageMessage": { "mimetype": "image/jpeg", "caption": '🔥 NEKOSMIC - BOT 🔥', "jpegThumbnail": fs.readFileSync(`./img1.jpeg`)}}}})}
 
 handler.command = /^(grupos|gruposofc|gruposofc)$/i
 module.exports = handler
