3401780b58205e489d4ea5cfb6a77638a6e7b664
NeKosmic-NK <104524366+NeKosmic-NK@users.noreply.github.com>
2022-05-01 18:02:15 +0100

    Update ruleta.js

--- plugins/ruleta.js
+++ plugins/ruleta.js
@@ -1,3 +1,20 @@
+let PhoneNumber = require('awesome-phonenumber')
+let levelling = require('../lib/levelling')
+
+let handler = async (m, { conn, usedPrefix }) => {
+
+  let pp = './Bot.jpeg'
+  let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+  try {
+//    pp = await conn.getProfilePicture(who)
+  } catch (e) {
+
+  } finally {
+    let about = (await conn.getStatus(who).catch(console.error) || {}).status || ''
+    let { name, limit, exp, banned, lastclaim, registered, regTime, age, level } = global.DATABASE.data.users[m.sender]
+    let { min, xp, max } = levelling.xpRange(level, global.multiplier)
+    let username = conn.getName(who)
+    let str = `
 case 'rolette':;case 'roleta':
                 if (!isxp) return await kill.reply(from, mess.needxpon(), id)
                 const limitrl = await gaming.getLimit(user, daily);if (gaming.isLimit(limitrl) == 1) return await kill.reply(from, mess.limitgame(), id)
