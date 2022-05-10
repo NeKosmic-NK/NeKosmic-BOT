312fa8419c5d40f4d1e84e44821ca466b850cd9c
NeKosmic-NK <104524366+NeKosmic-NK@users.noreply.github.com>
2022-05-10 12:16:14 +0100

    Update simsimi.js

--- plugins/simsimi.js
+++ plugins/simsimi.js
@@ -1,6 +1,6 @@
 let fetch = require('node-fetch')
 let handler = async (m, { text }) => {
-  let res = await fetch(global.API('https://api.cleverbot.com', '/v2/', { text: encodeURIComponent(text), lc: "es" }, ''))
+  let res = await fetch(global.API('https://api.simsimi.net', '/v2/', { text: encodeURIComponent(text), lc: "es" }, ''))
   let json = await res.json()
   if (json.success) m.reply(json.success)
   else throw json
